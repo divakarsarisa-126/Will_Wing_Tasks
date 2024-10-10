05fc47e7dded831f3c83db0cbae4ffc0754003fe
DIVAKAR_SARISA <divakarsarisa4175@gmail.com>
2024-10-10 18:22:53 +0530

    Update AcceptNumber_Modify.java

--- NumberPrograms2/AcceptNumber_Modify.java
+++ NumberPrograms2/AcceptNumber_Modify.java
@@ -6,26 +6,25 @@ public class AcceptNumber_Modify {
 
     public static int  modifyNumber(int number) {
     
-        String numStr = Integer.toString(number);
+     String numStr = Integer.toString(number);
 
-        StringBuilder modifiedNumber = new StringBuilder();
+    StringBuilder modifiedNumber = new StringBuilder();
 
-        for (int i = 0; i < numStr.length() - 1; i++) {
-          int digitone = Character.getNumericValue(numStr.charAt(i));
-          int digittwo = Character.getNumericValue(numStr.charAt(i + 1));
+    for (int i = 0; i < numStr.length() - 1; i++) {
+      int digitone = Character.getNumericValue(numStr.charAt(i));
+      int digittwo = Character.getNumericValue(numStr.charAt(i + 1));
 
-          int diff = Math.abs(digitone - digittwo);
-
-            modifiedNumber.append(diff);
-        }
-
-        modifiedNumber.append(numStr.charAt(numStr.length() - 1));
-
-        return Integer.parseInt(modifiedNumber.toString());
+      int diff = Math.abs(digitone - digittwo);
+        modifiedNumber.append(diff);
     }
 
+    modifiedNumber.append(numStr.charAt(numStr.length() - 1));
+
+    return Integer.parseInt(modifiedNumber.toString());
+}
+
     public static void main(String[] args) {
-    	Scanner scan = new Scanner(System.in);
+      Scanner scan = new Scanner(System.in);
     	System.out.print("Enter Number: ");
         int n =scan.nextInt();
   
