180103d31af66faca145d631a64e603b4b6ba454
DIVAKAR_SARISA <divakarsarisa4175@gmail.com>
2024-10-18 17:00:23 +0530

    Update Sum_evenNumdivisible_threeight.java

--- DiwaliSprint/Sum_evenNumdivisible_threeight.java
+++ DiwaliSprint/Sum_evenNumdivisible_threeight.java
@@ -2,24 +2,23 @@ package DiwaliSprint;
 
 public class Sum_evenNumdivisible_threeight {
 
-    public static int findSum(int n) {
-        int count = 0; 
-        int num = 0;   
+    public static int findSum(int num) {
+       
         int totalSum = 0; 
 
-        while (count < n) {
-            num++;
+        while (num > 0) {
             if (num % 2 == 0 && num % 3 == 0 && num % 8 == 0) {
                 totalSum += num;
-                count++;
+           
             }
+            num--;
         }
 
         return totalSum; 
     }
 
     public static void main(String[] args) {
-        int n = 5; 
+        int n = 24; 
         int result = findSum(n);
         System.out.println("The sum of the first " + n + " even numbers divisible by 3 and 8 is: " + result);
     }
